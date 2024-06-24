bc269686274448acfef6a048a24814778b8f9180
Witty Umosung <64681844+Pukkawit@users.noreply.github.com>
2024-06-24 18:45:23 +0100

    Furniro eCommerce-website

--- src/components/nav/site-logo/SiteLogo.jsx
+++ src/components/nav/site-logo/SiteLogo.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "../nav.scss";
+import { useNavigate } from "react-router-dom";
 
 const SiteLogo = () => {
+  const navigate = useNavigate();
+
+  const home = () => {
+    navigate("/");
+  };
   return (
-    <div className="styleSiteLogo">
+    <div className="styleSiteLogo" onClick={home} style={{ cursor: "pointer" }}>
       <img
         className="logoSize"
         src="../src/assets/icons/Meubel House_Logos-05.svg"
